3d5aceac67e1c310eb99926a4518ca84593a9967
MarioSeattle <marioseattle@live.com>
2020-05-26 11:37:42 -0600

    Use of JSON.parse to turn to data into something I can display

--- GiphySearch/javascript/main.js
+++ GiphySearch/javascript/main.js
@@ -1,30 +1,25 @@
 /* 1. Grab the input value */
 
 
-document.querySelector(".js-go").addEventListener("click", function(e){
+document.querySelector(".js-go").addEventListener('click',function(){
 
-	var input = document.querySelector("input").value;
-
-});
-
-//keyup to make it work with enter key
-
-document.querySelector(".js-userinput").addEventListener("keyup", function(e){
-
-	var input = document.querySelector("input").value;
-
-	//If enter key is press then execute
-	if(e.which === 13) {
-
-		pushToDOM(input);
-	}
-
+  var input = document.querySelector("input").value;
+  pushToDOM(input);
 
 });
 
+document.querySelector(".js-userinput").addEventListener('keyup',function(e){
+
+  var input = document.querySelector("input").value;
+
+  // if the key ENTER is pressed...
+  if(e.which === 13) {
+    pushToDOM(input);
+  }
+
+});
 
 /* 2. do the data stuff with the API */
-
 
 var url = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
 
@@ -33,37 +28,27 @@ var GiphyAJAXCall = new XMLHttpRequest();
 GiphyAJAXCall.open( 'GET', url );
 GiphyAJAXCall.send();
 
-GiphyAJAXCall.addEventListener("load", function(e){
+GiphyAJAXCall.addEventListener('load',function(e){
 
+  var data = e.target.response;
+  pushToDOM(data);
 
-	var data = e.target.response;
-	console.log(data);
-
-
-
-
-
-})
+});
 
 
 
 
 /* 3. Show me the GIFs */
 
-function pushToDOM (input) {
 
-	//alert("Esta Pasando?");
+function pushToDOM(input) {
 
-	var container = document.querySelector(".js-container");
-	container.innerHTML = input;
+  var response = JSON.parse(input);
+
+  var imageURL = response.data[0].images.fixed_height.url;
+  console.log(imageURL);
+
+  var container = document.querySelector(".js-container");
+  container.innerHTML = "<img src=\"https://media3.giphy.com/media/Z1kpfgtHmpWHS/200.gif\">";
 
 }
-
-
-
-
-
-
-
-
-
